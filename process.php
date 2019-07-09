751b60be58fb8659edfd09b4d9d1b298148cdb11
Pakjirayks <51689190+Pakjirayks@users.noreply.github.com>
2019-07-09 11:39:00 +0700

    Update process.php

--- process.php
+++ process.php
@@ -7,7 +7,7 @@ body {
     background-color:#600;
     color:#FFF;
 }
-
+-->
 </style></head>
 
 <body>
@@ -35,3 +35,9 @@ body {
 <div align="center"></div>
 </body>
 </html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ประมวลผล โปรแกรมคำนวณหาค่าดัชนีมวลกาย(BMI)</title>
+</head>
